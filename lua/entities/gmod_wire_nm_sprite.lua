f1d45e508e54d1e676cfb83a88a3827b7673089b
Mista-Tea <thatguythomas@hotmail.com>
2016-11-07 18:58:33 -0700

    Reorganizing, cleaning up, adding server cvars

--- lua/entities/gmod_wire_nm_sprite.lua
+++ lua/entities/gmod_wire_nm_sprite.lua
@@ -1,110 +1,181 @@
 
-DEFINE_BASECLASS( "base_wire_entity" )
-ENT.Author       = "NickMaps, PTugaSantos"
-ENT.PrintName       = "Sprite"
-ENT.RenderGroup		= RENDERGROUP_BOTH
-ENT.WireDebugName	= "Sprite"
+-- Wire Sprites by NickMBR and PTugaSantos
+-- See also: nm_sprites.lua
 
-if SERVER then
-	
-	AddCSLuaFile()
-	
-	function ENT:Initialize()
-		self:PhysicsInit( SOLID_VPHYSICS )
-		self:SetMoveType( MOVETYPE_VPHYSICS )
-		self:SetSolid( SOLID_VPHYSICS )
-		self:SetNoDraw( false )
-		self.Inputs = WireLib.CreateInputs( self, {"On", "RGB [VECTOR]", "Alpha", "Scale"} )
-	end
-	
-	function ENT:Setup( spr_scale, spr_framerate, spr_rendermode, spr_texture, r, g, b, a )
-		self.spr_scale					= math.Clamp(spr_scale, 0.01, 100)
-		self.spr_framerate		= math.Clamp(spr_framerate, 0, 30)
-		self.spr_rendermode	= spr_rendermode
-		self.spr_texture				= spr_texture
-		self.r								= math.Clamp(r, 0, 255)
-		self.g								= math.Clamp(g, 0, 255)
-		self.b								= math.Clamp(b, 0, 255)
-		self.a								= math.Clamp(a, 0, 255)
-		
-		self:PostInitialized()
-		self:UpdateSprite()
-	end
-	
-	function ENT:PostInitialized()
-		if IsValid( self.Sprite ) then self.Sprite:Remove()  end
-		self.Sprite = self:CreateNMSprite()
-	end
-	
-	function ENT:CreateNMSprite( pos, texture, rendermode, r, g, b, a, framerate, scale )
-		local spr = ents.Create( "env_sprite" )
-		
-			if not IsValid( spr ) then return end
-			spr:SetPos( self:GetPos() )
-			spr:SetMoveType( MOVETYPE_NONE )
-			
-			spr:SetSaveValue( "rendermode", self.spr_rendermode )
-			spr:SetSaveValue( "model", self.spr_texture )
-			spr:SetSaveValue( "rendercolor", string.format( "%i %i %i", self.r, self.g, self.b ) )
-			spr:SetSaveValue( "framerate", self.spr_framerate )
-			spr:SetSaveValue( "scale", self.spr_scale )
-			spr:SetKeyValue( "renderamt", self.a )
-			
-		spr:Spawn()
-		spr:Activate()
-		spr:SetParent( self )
-		
-		return spr
-	end
-	
-	function ENT:TriggerInput( trigger, value )
-		if ( trigger == "On" ) then self:ToggleSprite( value )
-		elseif ( trigger == "RGB" ) then self.r, self.g, self.b = value[1], value[2], value[3]
-		elseif ( trigger == "Alpha" ) then self.a = value
-		elseif ( trigger == "Scale" ) then self.spr_scale = value end
-		
-		self:UpdateSprite()
-	end
-	
-	function ENT:UpdateSprite()
-		local spr = self.Sprite
-		
-		if IsValid( spr ) then
-			spr:SetKeyValue( "rendercolor", string.format( "%i %i %i", self.r, self.g, self.b ) )
-			spr:SetKeyValue( "rendermode", self.spr_rendermode )
-			spr:SetKeyValue( "framerate", self.spr_framerate )
-			spr:SetKeyValue( "scale", self.spr_scale )
-			spr:SetKeyValue( "renderamt", self.a )
-		end
-	end
-	
-	function ENT:ToggleSprite( val )
-		
-		local spr = self.Sprite
-		if IsValid( spr ) then spr:SetNoDraw( val ~= 1 ) end
-		
-	end
-	
-	duplicator.RegisterEntityClass( "gmod_wire_nm_sprite", WireLib.MakeWireEnt, "Data", "spr_scale", "spr_framerate", "spr_rendermode", "spr_texture", "r", "g", "b", "a" )
-	
-	// Backwards compatibility
-	duplicator.RegisterEntityClass( "nm_sprites", function( ply, data, ... )
-		
-		data.Class = "gmod_wire_nm_sprite"
-		
-		local params = {
-			data.spr_scale,
-			data.spr_framerate,
-			data.spr_rendermode,
-			data.spr_texture,
-			data.r,
-			data.g,
-			data.b,
-			data.a
-		}
-		
-		return WireLib.MakeWireEnt( ply, data, unpack( params ) )
-		
-	end, "Data" )
-	
+DEFINE_BASECLASS( "base_wire_entity" )
+
+ENT.Author        = "NickMaps, PTugaSantos"
+ENT.PrintName     = "Sprite"
+ENT.RenderGroup   = RENDERGROUP_BOTH
+ENT.WireDebugName = "Sprite"
+
+-- Client portion ends here
+if CLIENT then return end
+
+
+
+AddCSLuaFile()
+
+local cvarMaxScale     = GetConVar( "wire_nm_sprites_max_scale" )
+local cvarMaxFramerate = GetConVar( "wire_nm_sprites_max_framerate")
+
+local MIN_SCALE     = 0.01
+local MIN_FRAMERATE = 0
+local MIN_COLOR     = 0
+local MAX_COLOR     = 255
+
+local function getClampedScale( num )      return math.Clamp( num, MIN_SCALE,     cvarMaxScale:GetInt() )     end
+local function getClampedFramerate( num )  return math.Clamp( num, MIN_FRAMERATE, cvarMaxFramerate:GetInt() ) end
+local function getClampedColor( num )      return math.Clamp( num, MIN_COLOR,     MAX_COLOR )                 end
+
+--[[--------------------------------------------------------------------------
+--
+-- 	ENT:Initialize()
+--
+--]]--
+function ENT:Initialize()
+	self:PhysicsInit( SOLID_VPHYSICS )
+	self:SetMoveType( MOVETYPE_VPHYSICS )
+	self:SetSolid( SOLID_VPHYSICS )
+	self:SetNoDraw( false )
+	self.Inputs = WireLib.CreateInputs( self, {"On", "RGB [VECTOR]", "Alpha", "Scale"} )
 end
+
+--[[--------------------------------------------------------------------------
+--
+-- 	ENT:Setup()
+--
+--]]--
+function ENT:Setup( spr_scale, spr_framerate, spr_rendermode, spr_texture, r, g, b, a )
+	self.spr_scale      = getClampedScale( spr_scale )
+	self.spr_framerate  = getClampedFramerate( spr_framerate )
+	self.spr_rendermode	= spr_rendermode
+	self.spr_texture    = spr_texture
+	self.r              = getClampedColor( r )
+	self.g              = getClampedColor( g )
+	self.b              = getClampedColor( b )
+	self.a              = getClampedColor( a )
+	
+	self:PostInitialized()
+	self:UpdateSprite()
+end
+
+--[[--------------------------------------------------------------------------
+--
+-- 	ENT:PostInitialized()
+--
+--]]--
+function ENT:PostInitialized()
+	if IsValid( self.Sprite ) then
+		self.Sprite:Remove()
+	end
+	
+	self.Sprite = self:CreateNMSprite()
+end
+
+--[[--------------------------------------------------------------------------
+--
+-- 	ENT:CreateNMSprite()
+--
+--]]--
+function ENT:CreateNMSprite( pos, texture, rendermode, r, g, b, a, framerate, scale )
+	local spr = ents.Create( "env_sprite" )
+	
+	if not IsValid( spr ) then return end
+	
+	r = getClampedColor( self.r )
+	g = getClampedColor( self.g )
+	b = getClampedColor( self.b )
+	a = getClampedColor( self.a )
+	
+	scale     = getClampedScale( self.spr_scale )
+	framerate = getClampedFramerate( self.spr_framerate )
+	
+	
+	spr:SetPos( self:GetPos() )
+	spr:SetMoveType( MOVETYPE_NONE )
+	
+	spr:SetSaveValue( "model",       self.spr_texture )
+	spr:SetSaveValue( "rendercolor", ("%i %i %i"):format( r, g, b ) )
+	spr:SetSaveValue( "rendermode",  self.spr_rendermode )
+	spr:SetSaveValue( "framerate",   framerate )
+	spr:SetSaveValue( "scale",       scale )
+	spr:SetKeyValue(  "renderamt",   a )
+		
+	spr:Spawn()
+	spr:Activate()
+	spr:SetParent( self )
+	
+	return spr
+end
+
+local triggers = {
+	On    = function( self, value ) self:ToggleSprite( value ) end,
+	Alpha = function( self, value ) self.a = getClampedColor( value ) end,
+	Scale = function( self, value ) self.spr_scale = getClampedScale( value ) end
+	RGB   = function( self, value )
+		self.r = getClampedColor( value[1] )
+		self.g = getClampedColor( value[2] )
+		self.b = getClampedColor( value[3] )
+	end,
+}
+
+--[[--------------------------------------------------------------------------
+--
+-- 	ENT:TriggerInput()
+--
+--]]--
+function ENT:TriggerInput( trigger, value )
+	if ( not triggers[trigger] ) then return end
+	
+	triggers[trigger]( self, value )
+	self:UpdateSprite()
+end
+
+--[[--------------------------------------------------------------------------
+--
+-- 	ENT:UpdateSprite()
+--
+--]]--
+function ENT:UpdateSprite()
+	if not IsValid( self.Sprite ) then return end
+	
+	self.Sprite:SetKeyValue( "rendercolor", ("%i %i %i"):format( self.r, self.g, self.b ) )
+	self.Sprite:SetKeyValue( "rendermode",  self.spr_rendermode )
+	self.Sprite:SetKeyValue( "framerate",   self.spr_framerate )
+	self.Sprite:SetKeyValue( "scale",       self.spr_scale )
+	self.Sprite:SetKeyValue( "renderamt",   self.a )
+end
+
+--[[--------------------------------------------------------------------------
+--
+-- 	ENT:ToggleSprite()
+--
+--]]--
+function ENT:ToggleSprite( val )
+	if not IsValid( self.Sprite ) then return end
+	
+	self.Sprite:SetNoDraw( val ~= 1 )
+end
+
+duplicator.RegisterEntityClass( "gmod_wire_nm_sprite", WireLib.MakeWireEnt, "Data", "spr_scale", "spr_framerate", "spr_rendermode", "spr_texture", "r", "g", "b", "a" )
+
+-- Backwards compatibility
+duplicator.RegisterEntityClass( "nm_sprites", function( ply, data, ... )
+	
+	data.Class = "gmod_wire_nm_sprite"
+	
+	local params = {
+		getClampedScale( data.spr_scale ),
+		getClampedFramerate( data.spr_framerate )
+		data.spr_rendermode,
+		data.spr_texture,
+		getClampedColor( data.r ),
+		getClampedColor( data.g ),
+		getClampedColor( data.b ),
+		getClampedColor( data.a )
+	}
+	
+	return WireLib.MakeWireEnt( ply, data, unpack( params ) )
+	
+end, "Data" )
--- lua/wire/stools/nm_sprites.lua
+++ lua/wire/stools/nm_sprites.lua
@@ -1,35 +1,48 @@
 
-// Wire Sprites by NickMBR and PTugaSantos
-// See also: gmod_wire_nm_sprite.lua
+-- Wire Sprites by NickMBR and PTugaSantos
+-- See also: gmod_wire_nm_sprite.lua
 
 WireToolSetup.setCategory( "Visuals" )
 WireToolSetup.open( "nm_sprites", "Sprite", "gmod_wire_nm_sprite", nil, "Sprites" )
+WireToolSetup.BaseLang()
+WireToolSetup.SetupMax( 25 )
+
+local mode       = "wire_"..TOOL.Mode
+local prefix     =  "tool."..mode.."."
+local prefixlang = "#tool."..mode.."."
+
+TOOL.Information = { "left" }
+
+local cvarFlags        = {FCVAR_REPLICATED, FCVAR_SERVER_CAN_EXECUTE, FCVAR_ARCHIVE}
+local cvarMaxScale     = CreateConVar( mode.."_max_scale",    100, cvarFlags, "Defines the maximum scale of a sprite" )
+local cvarMaxFramerate = CreateConVar( mode.."_max_framerate", 30, cvarFlags, "Defines the maximum framerate of a sprite" )
 
 if CLIENT then
-	// Tool Strings and ConVars
-	language.Add( "tool.wire_nm_sprites.name", "Wire Sprites" )
-	language.Add( "tool.wire_nm_sprites.desc", "Creates a customizable sprite entity." )
-	language.Add( "tool.wire_nm_sprites.0", "Primary: Create Sprite Entity" )
+
+	-- Tool Strings and ConVars
+	language.Add( prefix.."name", "Wire Sprites" )
+	language.Add( prefix.."desc", "Creates a customizable sprite entity." )
+	language.Add( prefix.."left", "Create Sprite Entity" )
 	
-	language.Add( "NMSprite_Scale", "Scale:" )
-	language.Add( "NMSprite_RenderMode", "Render Mode:" )
-	language.Add( "NMSprite_SpriteColor", "Color:" )
-	language.Add( "NMSprite_FrameRate", "Framerate:" )
-	language.Add( "NMSprite_SpriteText", "Texture:" )
+	language.Add( mode.."_scale",       "Scale:" )
+	language.Add( mode.."_rendermode",  "Render Mode:" )
+	language.Add( mode.."_spritecolor", "Color:" )
+	language.Add( mode.."_framerate",   "Framerate:" )
+	language.Add( mode.."_spritetext",  "Texture:" )
 	
 	TOOL.ClientConVar = {
-		model = "models/beer/wiremod/watersensor_mini.mdl",
-		spr_scale = 1,
+		model          = "models/beer/wiremod/watersensor_mini.mdl",
+		spr_scale      = 1,
 		spr_rendermode = 9,
-		spr_framerate = 10,
-		spr_texture = "sprites/light_glow03.vmt",
-		r = 0,
-		g = 0,
-		b = 0,
-		a = 0
+		spr_framerate  = 10,
+		spr_texture    = "sprites/light_glow03.vmt",
+		r              = 0,
+		g              = 0,
+		b              = 0,
+		a              = 0
 	}
 	
-	// List of sprites textures
+	-- List of sprites textures
 	local textures = {
 		"sprites/light_glow03.vmt",
 		"sprites/animglow01.vmt",
@@ -55,54 +68,64 @@ if CLIENT then
 		"sprites/fire1.vmt",
 		"sprites/fire2.vmt"
 	}
-	for key, texture in pairs( textures ) do  list.Set( "SpriteTextures", texture, true ) end
 	
-	// List of placeholders props
+	for key, texture in pairs( textures ) do
+		list.Set( "SpriteTextures", texture, true )
+	end
+	
+	-- List of placeholders props
 	list.Set( "NMSpriteModels", "models/beer/wiremod/watersensor_mini.mdl", true )
-	list.Set( "NMSpriteModels", "models/beer/wiremod/hydraulic_mini.mdl", true )
+	list.Set( "NMSpriteModels", "models/beer/wiremod/hydraulic_mini.mdl",   true )
 	
-	// CPanel Constructor
+	--[[--------------------------------------------------------------------------
+	--
+	-- 	TOOL.BuildCPanel()
+	--
+	--]]--
 	function TOOL.BuildCPanel( panel )
 		
-		// Header
-		WireToolHelpers.MakePresetControl( panel, "wire_nm_sprites" )
-		panel:AddControl( "Header", { Text = "#tool.wire_nm_sprites.name" }  )
+		-- Header
+		WireToolHelpers.MakePresetControl( panel, mode )
+		panel:AddControl( "Header", { Text = prefixlang.."name" }  )
 		
-		// List base models for sprite base
+		-- List base models for sprite base
 		local models = list.Get( "NMSpriteModels" )
-		WireDermaExts.ModelSelect( panel, "wire_nm_sprites_model", models, 1 )
+		WireDermaExts.ModelSelect( panel, mode.."_model", models, 1 )
 		
-		// List sprites from custom list
+		-- List sprites from custom list
 		local params = {
 			Label = "#Texture",
 			Height = "150",
 			MenuButton = "0",
 			Options = {}
 		}
-		for key, _ in pairs( list.Get( "SpriteTextures" ) ) do params.Options[key] = { wire_nm_sprites_spr_texture = key } end
+		
+		for key, _ in pairs( list.Get( "SpriteTextures" ) ) do
+			params.Options[key] = { wire_nm_sprites_spr_texture = key }
+		end
 		panel:AddControl( "ListBox", params )
 		
-		// Set Framerate and Scale of the sprite
-		panel:NumSlider( "#NMSprite_FrameRate", "wire_nm_sprites_spr_framerate", 0, 30, 0 )
-		panel:NumSlider( "#NMSprite_Scale", "wire_nm_sprites_spr_scale", 0, 100, 2 )
+		-- Set Framerate and Scale of the sprite
+		panel:NumSlider( prefixlang.."_framerate", mode.."_spr_framerate", 0,    cvarMaxFramerate:GetInt(), 0 )
+		panel:NumSlider( prefixlang.."_scale",     mode.."_spr_scale",     0.01, cvarMaxScale:GetInt(),     2 )
 		
-		// Sets the render mode of the sprite
+		-- Sets the render mode of the sprite
 		panel:AddControl( "ComboBox", {
-				Label = "#NMSprite_RenderMode",
+				Label = prefixlang.."_rendermode",
 				Options = {
-					["World Glow"] = { wire_nm_sprites_spr_rendermode = 9 },
-					Glow = { wire_nm_sprites_spr_rendermode = 3 }
+					["World Glow"] = { [mode.."_spr_rendermode"] = 9 },
+					Glow =           { [mode.."_spr_rendermode"] = 3 }
 				}
 			}
 		)
 		
-		// Adds a color mixer with alpha enabled.
+		-- Adds a color mixer with alpha enabled.
 		panel:AddControl( "Color", {
-				Label		= "#NMSprite_SpriteColor",
-				Red			= "wire_nm_sprites_r",
-				Green		= "wire_nm_sprites_g",
-				Blue		= "wire_nm_sprites_b",
-				Alpha		= "wire_nm_sprites_a",
+				Label		= prefixlang.."_spritecolor",
+				Red			= mode.."_r",
+				Green		= mode.."_g",
+				Blue		= mode.."_b",
+				Alpha		= mode.."_a",
 				ShowAlpha	= "1",
 				ShowHSV		= "1",
 				ShowRGB		= "1",
@@ -110,25 +133,24 @@ if CLIENT then
 			}
 		)
 	end
-end
+	
+elseif SERVER then
 
-WireToolSetup.BaseLang()
-WireToolSetup.SetupMax( 25 )
-
-if SERVER then
+	--[[--------------------------------------------------------------------------
+	--
+	-- 	TOOL:GetConVars()
+	--
+	--]]--
 	function TOOL:GetConVars()
-		
-		local rtn = {
-			self:GetClientNumber("spr_scale"),
-			self:GetClientNumber("spr_framerate"),
-			self:GetClientNumber("spr_rendermode"),
-			self:GetClientInfo("spr_texture"),
-			self:GetClientNumber("r"),
-			self:GetClientNumber("g"),
-			self:GetClientNumber("b"),
-			self:GetClientNumber("a")
-		}
-		
-		return unpack( rtn )
+		return unpack({
+			self:GetClientNumber( "spr_scale" ),
+			self:GetClientNumber( "spr_framerate" ),
+			self:GetClientNumber( "spr_rendermode" ),
+			self:GetClientInfo( "spr_texture" ),
+			self:GetClientNumber( "r" ),
+			self:GetClientNumber( "g" ),
+			self:GetClientNumber( "b" ),
+			self:GetClientNumber( "a" )
+		})
 	end
 end
